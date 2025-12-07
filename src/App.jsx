2d50a40dfd3933445b250b1e37ff0f27ce9d1cf1
kawser Bappy <bappydu2015@gmail.com>
2025-12-07 23:42:33 +0600

    Basic setup completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-text  text-5xl py-10 font-bold">
+        Click on the Vite and React logos to learn more
+      </p>
+      <p className="text-text/70 py-10">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
